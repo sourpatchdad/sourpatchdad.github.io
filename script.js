ad2ec90ba85de658351f5e96ffb9e68cb5b93230
sourpatchdad <nathan.b.nicholas@gmail.com>
2025-11-03 16:29:35 -0600

    Change Trakt API credentials in fetchRecentlyWatched
    Updated Trakt API credentials check with actual values.

--- script.js
+++ script.js
@@ -74,8 +74,8 @@ async function fetchRecentlyWatched() {
     const feedContainer = document.getElementById('trakt-feed');
 
     // Check if configuration is set
-    if (TRAKT_CONFIG.clientId === 'YOUR_TRAKT_CLIENT_ID' ||
-        TRAKT_CONFIG.username === 'YOUR_TRAKT_USERNAME') {
+    if (TRAKT_CONFIG.clientId === '92e0c311e18ec187627337bad034f1bc74a5274706090696caaa385ddc21fa8d' ||
+        TRAKT_CONFIG.username === 'sourpatchdad') {
         feedContainer.innerHTML = `
             <div class="error-message">
                 <p>To display your recently watched content, please configure your Trakt API credentials.</p>
